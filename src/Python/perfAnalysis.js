d193bc29410e155460610103abd1d2c1c2116f3a
Stephane Mbatchou <smbatchou@users.noreply.github.com>
2014-10-07 14:21:44 -0400

    Update perfAnalysis.js

--- src/Python/perfAnalysis.js
+++ src/Python/perfAnalysis.js
@@ -1,3 +1,3 @@
 //analysis the queries profiles of students collection in db school2 
 //=> return the latency of the slowest query
-db.profile.find({ns:'school2.students'}).sort({millis:-1}).pretty().limit(1).next().['millis']
+db.profile.find({ns:'school2.students'}).sort({millis:-1}).pretty().limit(1).next()['millis']
